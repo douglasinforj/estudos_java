301c55491e55b107ee58b293027031d440201cf4
Douglas Silva <douglasitpro@gmail.com>
2026-03-29 15:59:11 -0300

    Adicionando a Class principal para teste de execução

--- SistemaEscolar/SistemaEscolarApp.java
+++ SistemaEscolar/SistemaEscolarApp.java
@@ -1,8 +1,3 @@
-/* 
-Para estudos podemos escrever tudo em um arquivo só mas respeitando que só pode haver
-uma classe public
-*/  
-
 package SistemaEscolar;
 
 import java.util.ArrayList;
@@ -12,48 +7,39 @@ abstract class Pessoa {
     private String nome;
     private String registroDigital;
 
-    //metodo construtor da class
     public Pessoa(String nome, String registroDigital) {
         this.nome = nome;
         this.registroDigital = registroDigital;
     }
 
-    //encapsulamento (getters, setters)
     public String getNome(){
         return nome;
     }
+
     public String getRegistroDigital(){
         return registroDigital;
     }
 
-    //método abstrato
     public abstract void exibirDados();
-    //OBS.: Toda classe filha é obrigada a implementar esse método
 }
 
-
-
 class Aluno extends Pessoa {
-    //variavel que vai auxiliar na gestão de notas do aluno
+
     private List<Double> notas;
 
-    //metodo construtor da classe Aluno
     public Aluno(String nome, String matricula){
         super(nome, matricula);
-        //inicializando variável com Arraylist<>();
         this.notas = new ArrayList<>();
     }
 
-    //metodo para adicionar nota do aluno no arrayList notas
     public void adicionarNota(double nota){
-        if(nota >=0 && nota <=10){
+        if(nota >= 0 && nota <= 10){
             this.notas.add(nota);
-        }else{
+        } else {
             System.out.println("Erro: Nota inválida para o aluno " + getNome());
         }
     }
 
-    //metodo para calcular media do aluno
     public double calcularMedia(){
         if (notas.isEmpty()) return 0.0;
 
@@ -64,15 +50,28 @@ class Aluno extends Pessoa {
         return soma / notas.size();
     }
 
-    //metodo que garante que classes filhas possam sobrescrever sem criar uma nova.
     @Override
     public void exibirDados(){
         System.out.println("=== Ficha do Aluno ===");
         System.out.println("Nome: " + getNome());
         System.out.println("Matricula: " + getRegistroDigital());
         System.out.printf("Média Atual: %.2f%n", calcularMedia());
-        System.out.println("Status" + (calcularMedia() >= 7 ? "Aprovado" : "Em recuperação"));
+        System.out.println("Status: " + (calcularMedia() >= 7 ? "Aprovado" : "Em recuperação"));
     }
-
 }
 
+//Classe principal (única public)
+public class SistemaEscolarApp {
+
+    public static void main(String[] args){
+
+        Aluno aluno01 = new Aluno("Douglas Silva", "2026-java-01");
+
+        aluno01.adicionarNota(8.5);
+        aluno01.adicionarNota(7.0);
+        aluno01.adicionarNota(9.0);
+        aluno01.adicionarNota(3.0);
+
+        aluno01.exibirDados();
+    }
+}
