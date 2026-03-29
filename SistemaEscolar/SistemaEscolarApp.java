7ae2ff98c212cc1fdc9f202caa9ff42c368ebd6d
Douglas Silva <douglasitpro@gmail.com>
2026-03-29 12:19:29 -0300

    Iniciando com Class Pessoa, classe comum para todos o sistema
    Criado metodo construtor para a class Pessoa
    
    encapsulamento variaveis usando getters, setters
    
    criado metodo exibir dados, onde será exibidos dados diferentes para cada objeto criado

--- SistemaEscolar/SistemaEscolarApp.java
+++ SistemaEscolar/SistemaEscolarApp.java
@@ -3,9 +3,28 @@ Para estudos podemos escrever tudo em um arquivo só mas respeitando que só pod
 uma classe public
 */  
 
-
-
 package SistemaEscolar;
 
+abstract class Pessoa {
+    private String nome;
+    private String registroDigital;
 
-abstract class Pessoa
+    //metodo construtor da class
+    public Pessoa(String nome, String registroDigital) {
+        this.nome = nome;
+        this.registroDigital = registroDigital;
+    }
+
+    //encapsulamento (getters, setters)
+    public String getNome(){
+        return nome;
+    }
+    public String getRegistroDigital(){
+        return registroDigital;
+    }
+
+    //método abstrato
+    public abstract void exibirDados();
+    
+}
+
