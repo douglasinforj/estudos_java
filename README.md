8b5ef9198c6b596a068af4835dd9a5dc2708c103
Douglas Silva <douglasitpro@gmail.com>
2026-03-29 11:59:51 -0300

    iniciando com SistemaEscolarApp - Práticas de POO

---
+++ README.md
@@ -0,0 +1,1 @@
+# Estudos na liguagem java POO
---
+++ SistemaEscolar/SistemaEscolarApp.java
@@ -0,0 +1,11 @@
+/* 
+Para estudos podemos escrever tudo em um arquivo só mas respeitando que só pode haver
+uma classe public
+*/  
+
+
+
+package SistemaEscolar;
+
+
+abstract class Pessoa
