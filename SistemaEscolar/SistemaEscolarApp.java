c1874ce712c1a23f5deca14e3ba0edbe720c325e
Douglas Silva <douglasitpro@gmail.com>
2026-03-29 13:00:44 -0300

    Implementado class Aluno que herda de Pessoa
    Criado metodo construtor da classe para inicialização dos atributos quando instanciada
    
    inicializando atributos da classe pai super
    
    Atribuindo a variavel com contrato List para ArrayList para acesso rápido com por indices
    
    criado metodo para adicionar nota infomada ao ArrayList notas com validação minima das notas não podendo ser negativos ou maior que 10
    
    Criado metodo de calcularMedia do aluno, com validação caso o array esteja vazio, criando uma variavel inicialmente com o valor zero, caso não adicionei um for para interar os valor em notas para a variavel temporária nota, fazendo a soma e retornando calculo da media soma dividindo pela quantidade contadas de notas atribuidas com .size()

--- SistemaEscolar/SistemaEscolarApp.java
+++ SistemaEscolar/SistemaEscolarApp.java
@@ -5,6 +5,9 @@ uma classe public
 
 package SistemaEscolar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 abstract class Pessoa {
     private String nome;
     private String registroDigital;
@@ -25,6 +28,51 @@ abstract class Pessoa {
 
     //método abstrato
     public abstract void exibirDados();
-    
+    //OBS.: Toda classe filha é obrigada a implementar esse método
+}
+
+
+
+class Aluno extends Pessoa {
+    //variavel que vai auxiliar na gestão de notas do aluno
+    private List<Double> notas;
+
+    //metodo construtor da classe Aluno
+    public Aluno(String nome, String matricula){
+        super(nome, matricula);
+        //inicializando variável com Arraylist<>();
+        this.notas = new ArrayList<>();
+    }
+
+    //metodo para adicionar nota do aluno no arrayList notas
+    public void adicionarNota(double nota){
+        if(nota >=0 && nota <=10){
+            this.notas.add(nota);
+        }else{
+            System.out.println("Erro: Nota inválida para o aluno " + getNome());
+        }
+    }
+
+    //metodo para calcular media do aluno
+    public double calcularMedia(){
+        if (notas.isEmpty()) return 0.0;
+
+        double soma = 0;
+        for(double nota : notas){
+            soma += nota;
+        }
+        return soma / notas.size();
+    }
+
+    //metodo que garante que classes filhas possam sobrescrever sem criar uma nova.
+    @Override
+    public void exibirDados(){
+        System.out.println("=== Ficha do Aluno ===");
+        System.out.println("Nome: " + getNome());
+        System.out.println("Matricula: " + getRegistroDigital());
+        System.out.printf("Média Atual: %.2f%n", calcularMedia());
+        System.out.println("Status" + (calcularMedia() >= 7 ? "Aprovado" : "Em recuperação"));
+    }
+
 }
 
