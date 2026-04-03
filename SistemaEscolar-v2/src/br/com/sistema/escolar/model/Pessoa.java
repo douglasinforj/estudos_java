db54b4763dcc81f50e104ea9535ad2b1180d33ed
Douglas Silva <douglasitpro@gmail.com>
2026-04-02 22:21:20 -0300

    Implementando class Pessoa, esta classe contem atributos comuns do sistema possibilitando reutilização de código.

--- SistemaEscolar-v2/src/br/com/sistema/escolar/model/Pessoa.java
+++ SistemaEscolar-v2/src/br/com/sistema/escolar/model/Pessoa.java
@@ -1,5 +1,24 @@
 package br.com.sistema.escolar.model;
 
 public abstract class Pessoa {
+    private String nome;
+    private String registroDigital;
+
+    //Método construtor Pessoa
+    public Pessoa(String nome, String registroDigital){
+        this.nome = nome;
+        this.registroDigital = registroDigital;
+    }
+    //Encapsulamento
+    public String getNome(){
+        return nome;
+    }
+    public String getRegistroDigital(){
+        return registroDigital;
+    }
+    //Métodos abstratos que serão utilizados pelos objetos
+    public abstract void exibirDados();
+
+    public abstract String toCSV();
 
 }
